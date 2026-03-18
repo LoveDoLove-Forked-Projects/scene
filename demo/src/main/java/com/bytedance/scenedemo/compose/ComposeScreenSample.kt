04dfb3aa3ec37e91dd473641bad1164bade8e896
qii <qiibeta@gmail.com>
2026-03-18 12:51:44 +0800

    doc: add compose-bom and material3 to demo, update demo to Compose 1.7

--- demo/src/main/java/com/bytedance/scenedemo/compose/ComposeScreenSample.kt
+++ demo/src/main/java/com/bytedance/scenedemo/compose/ComposeScreenSample.kt
@@ -1,20 +1,32 @@
 package com.bytedance.scenedemo.compose
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleEventObserver
 import com.bytedance.scene.navigation.compose.ComposeScreen
 import com.bytedance.scene.navigation.compose.LocalNavigationScene
 import com.bytedance.scene.navigation.compose.LocalResultReceiver
 import com.bytedance.scene.navigation.compose.LocalScreenArguments
 import com.bytedance.scene.navigation.compose.pushCompose
-import com.google.accompanist.appcompattheme.AppCompatTheme
 
 class ComposeScreenSample : ComposeScreen() {
     @Composable
@@ -31,27 +43,56 @@ class ComposeScreenSample : ComposeScreen() {
                 lifecycleOwner.lifecycle.removeObserver(observer)
             }
         }
+        AppTheme {
+            SimpleScaffold()
+        }
+    }
 
-        AppCompatTheme {
-            Greeting()
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SimpleScaffold() {
+        val navigationScene = LocalNavigationScene.current
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("Sample Compose Page")
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        scrolledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                        navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                        actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+                    ),
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            navigationScene.pop()
+                        }) {
+                            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Menu")
+                        }
+                    },
+                )
+            }) { it ->
+            Box(
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    navigationScene.pushCompose(ComposeScreenSample().apply {
+                        setArguments(null)
+                    })
+                }) {
+                    Text("Click Me")
+                }
+            }
         }
     }
 
     @Composable
-    private fun Greeting() {
-        val navigationScene = LocalNavigationScene.current
-
-        Text(
-            text = "Test",
-            modifier = Modifier.Companion.padding(horizontal = 10.dp, vertical = 5.dp)
+    fun AppTheme(content: @Composable () -> Unit) {
+        val dark = isSystemInDarkTheme()
+        MaterialTheme(
+            colorScheme = if (dark) darkColorScheme() else lightColorScheme(), content = content
         )
-
-        Button(onClick = {
-            navigationScene.pushCompose(ComposeScreenSample().apply {
-                setArguments(null)
-            })
-        }) {
-            Text("Click")
-        }
     }
 }
